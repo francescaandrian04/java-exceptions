54eb632d4aa63d81e9e440a164ab8760402c8ca6
Francesca Andrian <francescaandrianaaa@gmail.com>
2025-03-08 17:10:50 +0200

    Modify commit.

--- Exceptii/src/Coeficient.java
+++ Exceptii/src/Coeficient.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Coeficient {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // solicitam user-ului sa introduca 2 integer
+        System.out.println("Enter two integers: ");
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+
+        System.out.println(number1 + " / " + number2 + " este " + (number1 + number2));
+
+    }
 }
