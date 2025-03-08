3510c4f8299d4093ba5cca5873adf6341d14d4ea
Francesca Andrian <francescaandrianaaa@gmail.com>
2025-03-08 17:15:13 +0200

    Modify.

--- Exceptii/src/Coeficient.java
+++ Exceptii/src/Coeficient.java
@@ -11,7 +11,10 @@ public class Coeficient {
         int number1 = input.nextInt();
         int number2 = input.nextInt();
 
-        System.out.println(number1 + " / " + number2 + " este " + (number1/number2));
+        if (number2 != 0)
+            System.out.println(number1 + " / " + number2 + " este " + (number1/number2));
+        else
+            System.out.println("Impartitorul/divizorul nu poate fi 0.");
 
     }
 }
